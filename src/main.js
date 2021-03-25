4ed052421d85b73cef7aa8e8d4167873df99f171
Edward Stoner <edwardlstoner@gmail.com>
2021-03-25 09:39:00 -0600

    Add GA tag

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "../src/assets/css/tailwind.css";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueGtag, {
+  config: { id: "G-5LB14SZ3S2" },
+});
 
 new Vue({
   render: (h) => h(App),
