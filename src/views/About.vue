a105f7d72f34629d31edac04bd70adbe00aa5b66
Edward Stoner <edwardlstoner@gmail.com>
2021-03-28 13:35:22 -0600

    Add more info to about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,9 +2,13 @@
   <div class="bg-gray-200 ctn">
     <div class="text mx-auto my-32 max-w-2xl leading-loose">
       <p class="pb-4">
+        This is an app that visualizes the traffic times on Interstate 70
+        between Denver and Vail, Colorado.
+      </p>
+      <p class="pb-4">
         This project was created by
         <a class="underline" href="https://www.edstoner.dev">Ed Stoner</a> using
-        Vue, TailwindCSS, Node, Express, MongoDB and Mongoose.
+        Vue, D3, TailwindCSS, Node, Express, MongoDB and Mongoose.
       </p>
       <p class="pb-4">
         You can check out the repo
